b0e0532986e51a9959276fa338b83e1befcf376e
Mauricio Cabrera <mauriciocabreragiron@MacBook-Pro-de-Mauricio.local>
2026-05-18 19:34:56 -0600

    Version 1 funcional con feature flag desactivada

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# FEATURE FLAG
+VERSION_2_ACTIVA = False
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    mensaje = ""
+
+    if request.method == "POST":
+
+        nombre = request.form["nombre"]
+
+        if VERSION_2_ACTIVA:
+            mensaje = f"Hello, {nombre}! - Version 2"
+        else:
+            mensaje = f"Hola, {nombre}! - Version 1"
+
+    # Mostrar pantalla dependiendo de la flag
+    if VERSION_2_ACTIVA:
+        return render_template("v2.html", mensaje=mensaje)
+
+    return render_template("v1.html", mensaje=mensaje)
+
+if __name__ == "__main__":
+    app.run(debug=True)
